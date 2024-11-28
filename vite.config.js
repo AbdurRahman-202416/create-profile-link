cd2878c86897e957da74bf4a165f5ffd40f63709
Abdur Rahman <abdurrahman19011@gmail.com>
2024-11-28 19:52:05 +0600

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"/create-profile-link",
   future: {
     // ... other flags
     v7_startTransition: true,
